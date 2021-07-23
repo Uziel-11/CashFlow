a9ebe955faa64f867f36d52ba4e256381bb8c9ce
Uziel <201274@ids.upchiapas.edu.mx>
2021-07-23 13:11:06 -0500

    vistas

---
+++ src/CashFlow/Controller/GestionController.java
@@ -0,0 +1,13 @@
+package CashFlow.Controller;
+
+import CashFlow.Main;
+import javafx.fxml.FXML;
+
+public class GestionController {
+
+
+    @FXML
+    void  regresarMenu(){
+        Main.setFXML("Menu", "Menu");
+    }
+}
---
+++ src/CashFlow/Controller/ReporteController.java
@@ -0,0 +1,12 @@
+package CashFlow.Controller;
+
+import CashFlow.Main;
+import javafx.fxml.FXML;
+
+public class ReporteController {
+
+    @FXML
+    void  regresarMenu(){
+        Main.setFXML("Menu", "Menu");
+    }
+}
