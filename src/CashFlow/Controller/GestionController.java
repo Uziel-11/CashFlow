8e1bc3e79fbd588d74c1009818a33bb6a333be15
luispaulo0 <193279@ids.upchiapas.edu.mx>
2021-07-25 17:08:57 -0500

    Vistas dinero

--- src/CashFlow/Controller/GestionController.java
+++ src/CashFlow/Controller/GestionController.java
@@ -2,12 +2,58 @@ package CashFlow.Controller;
 
 import CashFlow.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.awt.event.MouseEvent;
 
 public class GestionController {
 
+    @FXML
+    private TextField idNoSemanaCobrar;
 
     @FXML
-    void  regresarMenu(){
+    private TextField idRazonCobro;
+
+    @FXML
+    private TextField idMontoCobro;
+
+    @FXML
+    private TextField idNoSemanaPagar;
+
+    @FXML
+    private TextField idRazonPagar;
+
+    @FXML
+    private TextField idMontoPagar;
+
+    @FXML
+    private TextField idNoSemanaBanco;
+
+    @FXML
+    private TextField idRazonBanco;
+
+    @FXML
+    private TextField idMontoBanco;
+
+    @FXML
+    void GuardarBancoOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void GuardarCobroOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void GuardarPagoOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void regresarMenu(MouseEvent event) {
         Main.setFXML("Menu", "Menu");
     }
+
 }
+
